1a058931ab5931cdd5bff7ca1601789480fbadd8
shj <kan02134@gmail.com>
2021-01-15 22:00:30 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  ^~^
+</template>
+
+<script setup>
+import * as PIXI from 'pixi.js';
+</script>
+
+<script>
+console.log(PIXI);
+</script>
+
+<style>
+body {
+  margin: 0;
+  overflow: hidden;
+}
+</style>
