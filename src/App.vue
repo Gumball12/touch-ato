2c75f17360b16a8f28aa913d4b1b6c135a34b156
shj <kan02134@gmail.com>
2021-01-17 14:52:07 +0900

    Add link

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <span><a href="https://github.com/Gumball12/touch-ato" target="_blank">터치아토!</a></span>
+</template>
 
 <script>
 import render from './render';
@@ -10,5 +12,16 @@ body {
   margin: 0;
   overflow: hidden;
   background-color: #FFD891;
+}
+
+span {
+  position: fixed;
+  right: 1em;
+  bottom: 1em;
+}
+
+a {
+  color: #212121;
+  outline: 0;
 }
 </style>
